579153bc50a6b328778280a442f97e72da707bc0
Wraan <kamil.drewniak13@gmail.com>
2019-09-17 12:45:14 +0200

    changed the custom extention

---
+++ training/gensrc/org/training/constants/GeneratedTrainingConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Sep 17, 2019 11:53:49 AM                    ---
+ * ----------------------------------------------------------------
+ */
+package org.training.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedTrainingConstants
+{
+	public static final String EXTENSIONNAME = "training";
+	
+	protected GeneratedTrainingConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
